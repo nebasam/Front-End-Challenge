efad141756ad42307d19f9a515a667bec3aaaa32
Nebiyu Samuel <neba.samuel17@gmail.com>
2022-02-20 01:40:38 +0300

    initial work setup

---
+++ components/Display/pdf.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const pdf = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default pdf
---
+++ components/Rightpen/rightpen.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const rightpen = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default rightpen
