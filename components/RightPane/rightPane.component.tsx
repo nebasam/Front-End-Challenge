391145b84ce6c30ed341dce60117a0f088d6bfe9
Nebiyu Samuel <neba.samuel17@gmail.com>
2022-02-20 11:16:33 +0300

    finished rightPane component

--- components/RightPane/rightPane.component.tsx
+++ components/RightPane/rightPane.component.tsx
@@ -1,13 +1,34 @@
 import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
 import InputAdornment from "@mui/material/InputAdornment";
+import { styled } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import React, { useState } from "react";
+import { useStyles } from "./rightPane";
+
+const Input = styled("input")({
+  display: "none",
+});
 
 const RightPane = () => {
+  const classes = useStyles();
+
   // const classes = useStyles();
-  const [alignment, setAlignment] = React.useState();
-  const [field1, setField1] = useState();
+  const [field1, setField1] = React.useState();
+  const [field2, setField2] = React.useState();
+  const [field3, setField3] = React.useState();
+  let [loading, setLoading] = useState(false);
+
+  const handleChange = (event, newAlignment) => {
+    setField1(newAlignment);
+  };
+  const handleChange2 = (event, newAlignment) => {
+    setField2(newAlignment);
+  };
+  const handleChange3 = (event, newAlignment) => {
+    setField3(newAlignment);
+  };
 
   return (
     <div
@@ -19,15 +40,23 @@ const RightPane = () => {
       }}
     >
       {/* <h1>Right Pane Component</h1> */}
-      <h5>Files</h5>
-      <a href="">See history</a>
-      <input type="file" name="upload" id="file-select" />
+      <h5 className={classes.h5}>Files</h5>
+      <a href="" className={classes.link}>
+        See history
+      </a>
+      {/* <input type="file" name="upload" id="file-select" /> */}
+      <label htmlFor="contained-button-file">
+        <p className={classes.p}>No files attached</p>
+        <Input accept="*" id="contained-button-file" multiple type="file" />
+        {/* <Button variant="contained" component="span">
+          Upload
+        </Button> */}
+      </label>
       <hr width="100%" />
-      <h5>Mark</h5>
+      <h5 className={classes.h5}>Mark</h5>
       <TextField
         onChange={(e) => setField1(e.target.value)}
         size="small"
-        value={field1}
         id="outlined-basic"
         sx={{ m: 1, width: "18ch", height: "5ch" }}
         variant="outlined"
@@ -38,93 +67,172 @@ const RightPane = () => {
         }}
       />
       <hr width="100%" />
-      <h5>Rubric</h5>
-      <a href="">review and send</a>
+      <h5 className={classes.h5}>Rubric</h5>
+      <a href="" className={classes.link}>
+        Review and send
+      </a>
       <TextField
         size="small"
         id="outlined-basic"
         sx={{ m: 1, width: "25ch" }}
         variant="outlined"
+        value={field1}
         InputProps={{
           inputMode: "numeric",
           pattern: "[0-9]*",
           endAdornment: <InputAdornment position="end">/4</InputAdornment>,
         }}
       />{" "}
-      <ToggleButtonGroup color="primary" value="" exclusive>
+      <ToggleButtonGroup
+        color="primary"
+        backgroundColor="#000"
+        // backgroundColor="primary"
+        value={field1}
+        exclusive
+        onChange={handleChange}
+      >
         <ToggleButton
+          classes={{
+            selected: classes.toggleButton,
+          }}
           value="4"
           size="small"
           style={{ margin: 4, padding: "0.7rem 2rem" }}
         ></ToggleButton>
         <ToggleButton
+          classes={{
+            selected: classes.toggleButton,
+          }}
           value="3"
           size="small"
           style={{ margin: 4, padding: "0.7rem 2rem" }}
         ></ToggleButton>
         <ToggleButton
+          classes={{
+            selected: classes.toggleButton,
+          }}
           value="2"
           size="small"
           style={{ margin: 4, padding: "0.7rem 2rem" }}
         ></ToggleButton>
         <ToggleButton
+          classes={{
+            selected: classes.toggleButton,
+          }}
           value="1"
           size="small"
           style={{ margin: 4, padding: "0.7rem 2rem" }}
         ></ToggleButton>
       </ToggleButtonGroup>
       <hr width="100%" />
-      <h5>Clarity and Clearness</h5>
+      <a className={classes.link}>Clarity and Clearness</a>
       <TextField
         size="small"
         id="outlined-basic"
         sx={{ m: 1, width: "25ch" }}
         variant="outlined"
+        value={field2}
         InputProps={{
           inputMode: "numeric",
           pattern: "[0-9]*",
           endAdornment: <InputAdornment position="end">/4</InputAdornment>,
         }}
       />
-      <ButtonGroup
-        variant="contained"
-        aria-label="outlined primary button group"
+      <ToggleButtonGroup
+        color="primary"
+        value={field2}
+        exclusive
+        onChange={handleChange2}
       >
-        <Button size="small" style={{ margin: 4, padding: "0.7rem 2rem" }} />
-        <Button size="small" style={{ margin: 4, padding: "0.7rem 2rem" }} />
-        <Button size="small" style={{ margin: 4, padding: "0.7rem 2rem" }} />
-        <Button size="small" style={{ margin: 4, padding: "0.7rem 2rem" }} />
-      </ButtonGroup>
+        <ToggleButton
+          classes={{
+            selected: classes.toggleButton,
+          }}
+          value="4"
+          size="small"
+          style={{ margin: 4, padding: "0.7rem 2rem" }}
+        ></ToggleButton>
+        <ToggleButton
+          classes={{
+            selected: classes.toggleButton,
+          }}
+          value="3"
+          size="small"
+          style={{ margin: 4, padding: "0.7rem 2rem" }}
+        ></ToggleButton>
+        <ToggleButton
+          classes={{
+            selected: classes.toggleButton,
+          }}
+          value="2"
+          size="small"
+          style={{ margin: 4, padding: "0.7rem 2rem" }}
+        ></ToggleButton>
+        <ToggleButton
+          classes={{
+            selected: classes.toggleButton,
+          }}
+          value="1"
+          size="small"
+          style={{ margin: 4, padding: "0.7rem 2rem" }}
+        ></ToggleButton>
+      </ToggleButtonGroup>
       <hr width="100%" />
-      <h5>Conclusion</h5>
+      <a className={classes.link}>Conclusion</a>
       <TextField
         size="small"
         id="outlined-basic"
         sx={{ m: 1, width: "25ch" }}
         variant="outlined"
+        value={field3}
         InputProps={{
           inputMode: "numeric",
           pattern: "[0-9]*",
           endAdornment: <InputAdornment position="end">/3</InputAdornment>,
         }}
       />
-      <ButtonGroup
-        variant="contained"
-        aria-label="outlined primary button group"
+      <ToggleButtonGroup
+        color="primary"
+        value={field3}
+        exclusive
+        onChange={handleChange3}
       >
-        {/* <Button size="small" tyle={{ margin: 2 }} /> */}
-        <Button size="small" style={{ margin: 4, padding: "0.7rem 2rem" }} />
-        <Button size="small" style={{ margin: 4, padding: "0.7rem 2rem" }} />
-        <Button size="small" style={{ margin: 4, padding: "0.7rem 2rem" }} />
-      </ButtonGroup>
+        <ToggleButton
+          classes={{
+            selected: classes.toggleButton,
+          }}
+          value="3"
+          size="small"
+          style={{ margin: 4, padding: "0.7rem 2rem" }}
+        ></ToggleButton>
+        <ToggleButton
+          classes={{
+            selected: classes.toggleButton,
+          }}
+          value="2"
+          size="small"
+          style={{ margin: 4, padding: "0.7rem 2rem" }}
+        ></ToggleButton>
+        <ToggleButton
+          classes={{
+            selected: classes.toggleButton,
+          }}
+          value="1"
+          size="small"
+          style={{ margin: 4, padding: "0.7rem 2rem" }}
+        ></ToggleButton>
+      </ToggleButtonGroup>
       <hr width="100%" />
-      <h5>Private Comments</h5>
+      <a className={classes.link}>Private Comments</a>
       <TextField
         size="small"
         id="outlined-multiline-flexible"
+        // label="Multiline"
         multiline
         rows={2}
         placeholder="Add Private Comment"
+        // value={value}
+        // onChange={handleChange}
       />
       <Button variant="contained" style={{ width: 1, marginTop: "6px" }}>
         Post
