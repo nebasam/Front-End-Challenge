4bffc86f27b70d2ba0884207ea88d552a133e0ad
Nebiyu Samuel <neba.samuel17@gmail.com>
2022-02-20 11:29:53 +0300

    adjusted alignment for the left & right pane

--- components/Display/pdf.component.tsx
+++ components/Display/pdf.component.tsx
@@ -2,7 +2,15 @@ import React from "react";
 
 const pdf = () => {
   return (
-    <div>
+    <div
+      style={{
+        margin: "1rem",
+        flex: 4,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <h1>Main Display</h1>
     </div>
   );
