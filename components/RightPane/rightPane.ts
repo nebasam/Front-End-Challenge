795b7ac2bd8fbf676e2939b0f275345b8399676d
Nebiyu Samuel <neba.samuel17@gmail.com>
2022-02-20 10:10:28 +0300

    use styling for the rightPane

---
+++ components/RightPane/rightPane.ts
@@ -0,0 +1,20 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: any) => ({
+  h5: {
+    marginTop: 0,
+    marginBottom: 0,
+  },
+  toggleButton: { backgroundColor: "#1877F2 !important" },
+  link: {
+    textDecoration: "none",
+    fontSize: "0.9rem",
+    color: "#1877F2",
+  },
+  p: {
+    marginTop: 0,
+    marginBottom: 0,
+  },
+}));
+
+export { useStyles };
